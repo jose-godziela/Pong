941c68df4d69c41434650ac41e7f2bf015011208
Felix Godziela <jose.godziela_99@hotmail.com>
2019-10-16 10:44:02 -0300

    Added .bat files for optimization

---
+++ Clase-4/do_this_pls.bat
@@ -0,0 +1,3 @@
+copy %1lib\Raylib\lib\raylib.dll %2 /y
+mkdir %2resources
+xcopy %1Clase-4\resources %2\resources /s /e /y
---
+++ gitEmail.bat
@@ -0,0 +1,1 @@
+git config --global user.email "jose.godziela_99@hotmail.com" 
